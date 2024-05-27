7e863c6306fde5d545bec5db41f501402b603649
OwnerTheTud <142199201+OwnerTheTud@users.noreply.github.com>
2024-05-27 12:55:51 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const whatsapp = document.getElementById('whatsapp');
+const telegram = document.getElementById('telegram');
+
+telegram.addEventListener('click', ()=>{
+    location.href = 'https://www.t.me/rahimov_sh'
+});
+
+
+whatsapp.addEventListener('click', ()=>{
+    location.href = 'https://www.wa.link/jr3b35'
+})
+
